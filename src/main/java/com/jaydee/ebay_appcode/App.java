dfe895b1a4d94455b46689629c6e7201d6632952
Babajide Opeyemi <babajide.ogunluyi@gmail.com>
2023-08-22 21:43:18 -0400

    ebay commit

---
+++ src/main/java/com/jaydee/ebay_appcode/App.java
@@ -0,0 +1,13 @@
+package com.jaydee.ebay_appcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
